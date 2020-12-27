38dbf3b655624d61e6a3dfdf2928d7c47a83c678
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2020-12-27 21:59:48 +0400

    checking error with github pages

---
+++ js/scrolleffect.js
@@ -0,0 +1,64 @@
+$(function () {
+  // wait for document ready
+  // init
+  var controller = new ScrollMagic.Controller();
+  // define movement of panels
+  var wipeAnimation = new TimelineMax()
+    .fromTo(
+      "div.panel.lang",
+      1,
+      { x: "100%" },
+      { x: "0%", ease: Linear.easeNone, duration: 1 }
+    ) // in from right
+    .fromTo(
+      "div.panel.tech",
+      1,
+      { x: "100%" },
+      { x: "0%", ease: Linear.easeNone, duration: 1 }
+    ) // in from right
+    .fromTo(
+      "div.panel.project_intro_page",
+      1,
+      { x: "-100%" },
+      { x: "0%", ease: Linear.easeNone, duration: 1 }
+    ) // in from left
+    .fromTo(
+      "div.panel.second",
+      1,
+      { x: "-100%" },
+      { x: "0%", ease: Linear.easeNone, duration: 1 }
+    ) // in from left
+    .fromTo(
+      "div.panel.work_intro",
+      1,
+      { x: "100%" },
+      { x: "0%", ease: Linear.easeNone, duration: 1 }
+    )
+    .fromTo(
+      "div.panel.third",
+      1,
+      { x: "100%" },
+      { x: "0%", ease: Linear.easeNone, duration: 1 }
+    ) // in from right
+    .fromTo(
+      "div.panel.third_next",
+      1,
+      { x: "100%" },
+      { x: "0%", ease: Linear.easeNone, duration: 1 }
+    ) // in from right
+    .fromTo(
+      "div.panel.fourth",
+      1,
+      { y: "-100%" },
+      { y: "0%", ease: Linear.easeNone }
+    ); // in from top
+  // create scene to pin and link animation
+  new ScrollMagic.Scene({
+    triggerElement: "#pinContainer",
+    triggerHook: "onLeave",
+    duration: "300%",
+  })
+    .setPin("#pinContainer")
+    .setTween(wipeAnimation)
+    .addTo(controller);
+});
