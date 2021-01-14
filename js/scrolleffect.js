cd3c71ddc7ac91b9d972708e3b14015694c3cd8b
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2021-01-14 16:37:12 +0400

    added styling

--- js/scrolleffect.js
+++ js/scrolleffect.js
@@ -8,49 +8,49 @@ $(function () {
       "div.panel.lang",
       1,
       { x: "100%" },
-      { x: "0%", ease: Linear.easeNone, duration: 1 }
+      { x: "0%", ease: Linear.easeInOut, duration: 100 }
     ) // in from right
     .fromTo(
       "div.panel.tech",
       1,
       { x: "100%" },
-      { x: "0%", ease: Linear.easeNone, duration: 1 }
+      { x: "0%", ease: Linear.easeInOut, duration: 100 }
     ) // in from right
     .fromTo(
       "div.panel.project_intro_page",
       1,
       { x: "-100%" },
-      { x: "0%", ease: Linear.easeNone, duration: 1 }
+      { x: "0%", ease: Linear.easeInOut, duration: 100 }
     ) // in from left
     .fromTo(
       "div.panel.second",
       1,
       { x: "-100%" },
-      { x: "0%", ease: Linear.easeNone, duration: 1 }
+      { x: "0%", ease: Linear.easeInOut, duration: 100 }
     ) // in from left
     .fromTo(
       "div.panel.work_intro",
       1,
       { x: "100%" },
-      { x: "0%", ease: Linear.easeNone, duration: 1 }
+      { x: "0%", ease: Linear.easeInOut, duration: 100 }
     )
     .fromTo(
       "div.panel.third",
       1,
       { x: "100%" },
-      { x: "0%", ease: Linear.easeNone, duration: 1 }
+      { x: "0%", ease: Linear.easeInOut, duration: 100 }
     ) // in from right
     .fromTo(
       "div.panel.third_next",
       1,
       { x: "100%" },
-      { x: "0%", ease: Linear.easeNone, duration: 1 }
+      { x: "0%", ease: Linear.easeInOut, duration: 1 }
     ) // in from right
     .fromTo(
       "div.panel.fourth",
       1,
       { y: "-100%" },
-      { y: "0%", ease: Linear.easeNone }
+      { y: "0%", ease: Linear.easeInOut }
     ); // in from top
   // create scene to pin and link animation
   new ScrollMagic.Scene({
